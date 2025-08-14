9511c1aef901b2349fc1ebb7590f3b318c32e33f
ispong <ispong@outlook.com>
2025-08-14 10:10:50 +0800

    本地模型支持重复对话

--- torch-yun-backend/torch-yun-api/src/main/java/com/isxcode/torch/api/agent/req/ChatAgentAiContent.java
+++ torch-yun-backend/torch-yun-api/src/main/java/com/isxcode/torch/api/agent/req/ChatAgentAiContent.java
@@ -1,9 +1,12 @@
 package com.isxcode.torch.api.agent.req;
 
+import com.isxcode.torch.api.chat.dto.ChatContent;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class ChatAgentAiContent {
 
-    private String prompt;
+    private List<ChatContent> messages;
 }
