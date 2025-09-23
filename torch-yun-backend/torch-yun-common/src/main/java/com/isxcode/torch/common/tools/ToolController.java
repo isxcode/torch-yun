d47e059c813ae863b684e1fc9926e3408dd4c0d3
ispong <ispong@outlook.com>
2025-09-23 11:14:34 +0800

    统一tool工具

--- torch-yun-backend/torch-yun-common/src/main/java/com/isxcode/torch/common/tools/ToolController.java
+++ torch-yun-backend/torch-yun-common/src/main/java/com/isxcode/torch/common/tools/ToolController.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.Strings;
 import org.jasypt.util.text.AES256TextEncryptor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.logging.LogLevel;
 import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.cache.CacheManager;
@@ -31,7 +30,8 @@ import java.util.Objects;
 
 import static java.net.URLConnection.guessContentTypeFromName;
 
-@Tag(name = "工具模块")
+
+@Tag(name = "系统内部工具模块")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/tools")
@@ -41,15 +41,18 @@ public class ToolController {
     private final CacheManager cacheManager;
 
     private final ResourceLoader resourceLoader;
-
-    private final SecurityProperties securityProperties;
+    private final IsxAppProperties isxAppProperties;
 
     @Value("${jasypt.encryptor.password}")
     private String jasyptPassword;
 
-    private final IsxAppProperties isxAppProperties;
+    @Value("${jasypt.encryptor.property.suffix}")
+    private String jasyptSuffix;
 
-    @Operation(summary = "获取版本号接口")
+    @Value("${jasypt.encryptor.property.prefix}")
+    private String jasyptPrefix;
+
+    @Operation(summary = "获取当前版本信息接口")
     @GetMapping("/open/version")
     public String getLeoLastVersion() {
 
@@ -70,43 +73,14 @@ public class ToolController {
         }
     }
 
-    @Operation(summary = "切换系统日志等级")
-    @GetMapping("/open/setLogLevel")
-    public String setLogLevel(@RequestParam String level, @RequestParam String name, @RequestParam String password) {
-
-        if (securityProperties.getUser().getName().equals(name)
-            && securityProperties.getUser().getPassword().equals(password)) {
-            LoggingSystem system = LoggingSystem.get(LoggingSystem.class.getClassLoader());
-            LogLevel logLevel = LogLevel.valueOf(level.trim().toUpperCase(Locale.ENGLISH));
-            system.setLogLevel("com.isxcode.acorn", logLevel);
-            return "当前日志等级：" + logLevel.name();
-        } else {
-            return "没有权限";
-        }
-    }
-
-    @Operation(summary = "获取缓存列表")
-    @GetMapping("/getCacheList")
-    public String getCacheList() {
-
-        return cacheManager.getCacheNames().toString();
-    }
-
-    @Operation(summary = "获取制定缓存信息")
-    @GetMapping("/getCache")
-    public String getCache(@RequestParam String name) {
-
-        return Objects.requireNonNull(cacheManager.getCache(name)).getNativeCache().toString();
-    }
-
-    @Operation(summary = "返回当前应用状态")
+    @Operation(summary = "健康检查接口")
     @GetMapping("/open/health")
     public String health() {
 
         return "UP";
     }
 
-    @Operation(summary = "下载文件")
+    @Operation(summary = "健康检查接口")
     @GetMapping("/open/file/{fileName}")
     public ResponseEntity<Resource> file(@PathVariable String fileName) {
 
@@ -152,12 +126,36 @@ public class ToolController {
         }
     }
 
-    @Operation(summary = "jasypt加密工具")
-    @GetMapping("/open/jasyptEncrypt")
+    @Operation(summary = "切换系统日志等级接口")
+    @GetMapping("/developer/setLogLevel")
+    public String setLogLevel(@RequestParam String level) {
+
+        LoggingSystem system = LoggingSystem.get(LoggingSystem.class.getClassLoader());
+        LogLevel logLevel = LogLevel.valueOf(level.trim().toUpperCase(Locale.ENGLISH));
+        system.setLogLevel("com.isxcode.acorn", logLevel);
+        return "当前日志等级：" + logLevel.name();
+    }
+
+    @Operation(summary = "获取缓存列表接口")
+    @GetMapping("/developer/getCacheList")
+    public String getCacheList() {
+
+        return cacheManager.getCacheNames().toString();
+    }
+
+    @Operation(summary = "获取指定缓存信息接口")
+    @GetMapping("/developer/getCache")
+    public String getCache(@RequestParam String name) {
+
+        return Objects.requireNonNull(cacheManager.getCache(name)).getNativeCache().toString();
+    }
+
+    @Operation(summary = "jasypt加密工具接口")
+    @GetMapping("/developer/jasyptEncrypt")
     public String jasypt(@RequestParam String text) {
 
         AES256TextEncryptor encryptor = new AES256TextEncryptor();
         encryptor.setPassword(jasyptPassword);
-        return encryptor.encrypt(text);
+        return "加密后配置参数：" + jasyptPrefix + encryptor.encrypt(text) + jasyptSuffix;
     }
 }
