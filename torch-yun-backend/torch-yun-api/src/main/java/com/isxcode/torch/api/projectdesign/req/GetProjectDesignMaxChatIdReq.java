3cbcfa77ded53782df87905ca4bc0ad4a2bde48b
ispong <ispong@outlook.com>
2026-05-14 15:51:55 +0800

    新增项目设计接口

---
+++ torch-yun-backend/torch-yun-api/src/main/java/com/isxcode/torch/api/projectdesign/req/GetProjectDesignMaxChatIdReq.java
@@ -0,0 +1,20 @@
+package com.isxcode.torch.api.projectdesign.req;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class GetProjectDesignMaxChatIdReq {
+
+    @Schema(title = "项目ID", example = "ty_project_1")
+    @NotEmpty(message = "项目ID不能为空")
+    private String projectId;
+
+    @Schema(title = "会话ID", example = "ty_chat_1")
+    private String chatId;
+
+    @Schema(title = "会话类型", example = "PROD")
+    private String chatType;
+}
--- torch-yun-frontend/src/views/software-development/project-design/project-design.config.ts
+++ torch-yun-frontend/src/views/software-development/project-design/project-design.config.ts
@@ -3,7 +3,7 @@ export const BreadCrumbList = [{ name: '项目设计', code: 'project-assets' }]
 export const TableConfig: any = {
   tableData: [],
   colConfigs: [
-    { prop: 'name', title: '名称', minWidth: 180, showOverflowTooltip: true },
+    { prop: 'name', title: '名称', minWidth: 180, customSlot: 'nameSlot', showOverflowTooltip: true },
     { prop: 'remark', title: '备注', minWidth: 220, showOverflowTooltip: true },
     { prop: 'createByUsername', title: '创建人', minWidth: 140, showOverflowTooltip: true },
     { prop: 'createDateTime', title: '创建时间', minWidth: 160, showOverflowTooltip: true },
