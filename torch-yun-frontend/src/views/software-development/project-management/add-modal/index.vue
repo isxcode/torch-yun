93c3a1fda70fdf946d2a6d03f689acf9bec2cf48
ispong <ispong@outlook.com>
2026-05-14 15:07:50 +0800

    修改菜单

--- torch-yun-frontend/src/views/software-development/project-management/add-modal/index.vue
+++ torch-yun-frontend/src/views/software-development/project-management/add-modal/index.vue
@@ -4,9 +4,9 @@
       <el-form-item label="项目名" prop="name"><el-input v-model="formData.name" maxlength="200" placeholder="请输入" /></el-form-item>
       <el-form-item label="工作空间" prop="workspace"><el-input v-model="formData.workspace" maxlength="500" placeholder="请输入" /></el-form-item>
       <el-form-item label="资产目录" prop="assetsDir"><el-input v-model="formData.assetsDir" maxlength="500" placeholder="请输入" /></el-form-item>
-      <el-form-item label="项目设计" prop="designAppId"><el-select v-model="formData.designAppId" placeholder="请选择"><el-option v-for="item in appList" :key="item.id" :label="item.name" :value="item.id" /></el-select></el-form-item>
-      <el-form-item label="项目计划" prop="planAppId"><el-select v-model="formData.planAppId" placeholder="请选择"><el-option v-for="item in appList" :key="item.id" :label="item.name" :value="item.id" /></el-select></el-form-item>
-      <el-form-item label="项目开发" prop="developAppId"><el-select v-model="formData.developAppId" placeholder="请选择"><el-option v-for="item in appList" :key="item.id" :label="item.name" :value="item.id" /></el-select></el-form-item>
+      <el-form-item label="设计模型应用" prop="designAppId"><el-select v-model="formData.designAppId" placeholder="请选择"><el-option v-for="item in appList" :key="item.id" :label="item.name" :value="item.id" /></el-select></el-form-item>
+      <el-form-item label="计划模型应用" prop="planAppId"><el-select v-model="formData.planAppId" placeholder="请选择"><el-option v-for="item in appList" :key="item.id" :label="item.name" :value="item.id" /></el-select></el-form-item>
+      <el-form-item label="开发模型应用" prop="developAppId"><el-select v-model="formData.developAppId" placeholder="请选择"><el-option v-for="item in appList" :key="item.id" :label="item.name" :value="item.id" /></el-select></el-form-item>
       <el-form-item label="备注"><el-input v-model="formData.remark" show-word-limit type="textarea" maxlength="200" :autosize="{ minRows: 4, maxRows: 4 }" placeholder="请输入" /></el-form-item>
     </el-form>
   </BlockModal>
